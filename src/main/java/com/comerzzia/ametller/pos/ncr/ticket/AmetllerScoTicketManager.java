babc705f7112f73f2e0a623639eb0da4870e265a
AlvaroLS01 <136437203+AlvaroLS01@users.noreply.github.com>
2025-09-16 10:42:44 +0200

    Revert "Ajusta mensajes NCR para descuento 25%"

--- src/main/java/com/comerzzia/ametller/pos/ncr/ticket/AmetllerScoTicketManager.java
+++ src/main/java/com/comerzzia/ametller/pos/ncr/ticket/AmetllerScoTicketManager.java
@@ -1,8 +1,6 @@
 package com.comerzzia.ametller.pos.ncr.ticket;
 
 import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -16,28 +14,10 @@ import com.comerzzia.pos.services.ticket.lineas.LineaTicketAbstract;
 public class AmetllerScoTicketManager extends ScoTicketManager {
 
     private static final BigDecimal DESCUENTO25 = new BigDecimal("25.00");
-    private final Set<Integer> lineasConDescuento25 = new HashSet<>();
     private boolean descuento25Activo = false;
 
     public void setDescuento25Activo(boolean activo) {
         this.descuento25Activo = activo;
-    }
-
-    public boolean isLineaConDescuento25(LineaTicket linea) {
-        return linea != null && lineasConDescuento25.contains(linea.getIdLinea());
-    }
-
-    @Override
-    public void initSession() {
-        super.initSession();
-        lineasConDescuento25.clear();
-        descuento25Activo = false;
-    }
-
-    @Override
-    public void ticketInitilize() {
-        super.ticketInitilize();
-        lineasConDescuento25.clear();
     }
 
     @Override
@@ -45,15 +25,8 @@ public class AmetllerScoTicketManager extends ScoTicketManager {
         LineaTicket added = super.addLineToTicket(linea);
         if (descuento25Activo && added != null) {
             added.setDescuentoManual(DESCUENTO25);
-            lineasConDescuento25.add(added.getIdLinea());
             recalculateTicket();
         }
         return added;
-    }
-
-    @Override
-    public void deleteTicketLine(Integer idLinea) {
-        super.deleteTicketLine(idLinea);
-        lineasConDescuento25.remove(idLinea);
     }
 }
